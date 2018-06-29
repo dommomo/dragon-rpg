de030e8582d42873606bf058f6cb2acbe30cb579
Dom Ryan <dom1600@hotmail.com>
2018-06-29 14:57:10 +0930

    projectiles discriminate with damage

--- Assets/Weapons/Projectile.cs
+++ Assets/Weapons/Projectile.cs
@@ -1,12 +1,21 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour {
 
-    public float projectileSpeed; // Note other classes can set
+    const float DESTROY_DELAY = 0.01f;
+
+    [SerializeField] float projectileSpeed;
+    [SerializeField] GameObject shooter;    //inspectable when paused
 
     float damageCaused;
+
+    public void SetShooter(GameObject shooter)
+    {
+        this.shooter = shooter;
+    }
 
     public void SetDamage(float damage)
     {
@@ -15,11 +24,26 @@ public class Projectile : MonoBehaviour {
 
     void OnCollisionEnter(Collision collision)
     {
-        Component damagableComponent = collision.gameObject.GetComponent(typeof(IDamageable));
+        var collisionObject = collision.gameObject;
+        if (!GameObject.Equals(collisionObject.layer, shooter.layer))    //projectiles discriminate between layers
+        {
+            DamageIfDamageable(collisionObject);
+        }
+
+    }
+
+    private void DamageIfDamageable(GameObject collisionObject)
+    {
+        Component damagableComponent = collisionObject.GetComponent(typeof(IDamageable));
         if (damagableComponent)
         {
             (damagableComponent as IDamageable).TakeDamage(damageCaused);
         }
-        Destroy(gameObject, 0.01f);
+        Destroy(gameObject, DESTROY_DELAY);
+    }
+
+    internal float GetDefaultLaunchSpeed()
+    {
+        return projectileSpeed;
     }
 }
